9476b63c7dfd9276ed048d34eec9e40ffb0e6d95
Vanzhere <vaniya.sharma158@gmail.com>
2026-07-16 11:52:20 +0000

    Initial Commit

--- primecheck.java
+++ primecheck.java
@@ -35,3 +35,31 @@ System.out.println(isPrime(100));  // false
 }
 
 // javac primecheck.java && java primecheck
+
+// 204 count primes
+class Solution {
+    public int countPrimes(int n) {
+
+        if(n <= 2)return 0;
+
+        boolean[] isPrime = new boolean[n];
+        Arrays.fill(isPrime, true);
+
+        isPrime[0]= false;
+        isPrime[1]= false;
+
+        for(int i =2; i*i < n; i++){
+            if(isPrime[i]){
+                for(int j= i*i; j<n; j+=i){
+                    isPrime[j]= false;
+                }
+            }
+        }
+        int count = 0;
+        for(int i=2; i<n;i++){
+            if(isPrime[i]) count++;
+        }
+        return count;
+        
+    }
+}
