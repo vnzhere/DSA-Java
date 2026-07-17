a55bb744a5bcfe2f31a3ad797dab53d390900562
Vanzhere <vaniya.sharma158@gmail.com>
2026-07-17 11:58:33 +0000

    Initial COmmit

---
+++ Printalldivisor.java
@@ -0,0 +1,25 @@
+public class Printalldivisor{
+    static void printDivisors(int n){
+
+        System.out.println("divisors of" + n + ": ");
+        for(int i = 1; i*i<= n; i++){
+            if(n%i == 0){
+                System.out.print(i + " ");
+
+                if(i!= n/i){
+                    System.out.print(n/i + " ");
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+    System.out.println("=== Print Divisors ===");
+    printDivisors(36);   // 1 36 2 18 3 12 4 9 6
+    printDivisors(12);   // 1 12 2 6 3 
+    printDivisors(1);    // 1
+    printDivisors(7);    // 1 7
+    printDivisors(100);  // 1 100 2 50 4 25 5 20 10
+    }
+}
