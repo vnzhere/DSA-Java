ef505bd4d41ddd327031dd7b5a67e6dec2ebde59
Vanzhere <vaniya.sharma158@gmail.com>
2026-07-28 08:01:34 +0000

    update

--- GCD.java
+++ GCD.java
@@ -10,3 +10,25 @@ while(b!=0){
 if(b ==0) return a;
 return gcd (b , a% b);
 
+// LC 1979
+class Solution {
+    public int findGCD(int[] nums) {
+        int min = nums[0];
+        int max = nums[0];
+
+        for(int num:nums){
+            if(num<min) min=num;
+            if(num>max) max=num;
+        }
+        return GCD(min,max);
+        
+    }
+    private int GCD(int a, int b){
+        while(b!= 0){
+            int temp =b;
+            b = a%b;
+            a= temp;
+        }
+        return a;
+    }
+}
