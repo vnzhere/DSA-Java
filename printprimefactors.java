32cec9d4789438acb1554bbe7265b573aeb9adfc
Vanzhere <vaniya.sharma158@gmail.com>
2026-07-28 07:54:50 +0000

    Initial Commit

--- printprimefactors.java
+++ printprimefactors.java
@@ -30,3 +30,20 @@ class Solution {
 }
 
 // LC 650
+class Solution {
+    public int minSteps(int n) {
+        if(n == 1) return 0;
+        int steps =0;
+        for(int i =2; i*i<=n; i++){
+            while(n%i ==0){
+                steps+=i;
+                n=n/i;
+            }
+        }
+        if(n>1){
+            steps+=n;
+        }
+        return steps;
+        
+    }
+}
