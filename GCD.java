b51dd67f08c3c4edafb3daa0b102844d8acc59d9
Vanzhere <vaniya.sharma158@gmail.com>
2026-07-28 08:02:12 +0000

    update

--- GCD.java
+++ GCD.java
@@ -32,3 +32,23 @@ class Solution {
         return a;
     }
 }
+
+// LC 1071
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if(!(str1 + str2) .equals(str2 + str1)){
+            return "";
+        }
+        int gcdlength = gcd(str1.length(), str2.length());
+        return str1.substring(0, gcdlength);
+        
+    }
+    private int gcd(int a, int b){
+        while(b!=0){
+           int temp = b;
+           b= a % b;
+           a = temp; 
+        }
+        return a;
+    }
+}
