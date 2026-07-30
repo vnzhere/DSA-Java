9813130cf322a4d383007e98037ff30700cb3531
Vanzhere <vaniya.sharma158@gmail.com>
2026-07-30 03:24:44 +0000

    update

---
+++ powerexponent.java
@@ -0,0 +1,16 @@
+long N = n;
+if(N < 0){
+    x = 1/x;
+    N = -N;
+}
+
+double result = 1;
+
+while(N > 0){
+    if( N%2 == 1){
+        result *= x;
+    }
+    x *= x;
+    N = N/2;
+}
+return result;
