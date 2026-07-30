2d18b802c908fab40d7b36f8a5d8c5aac032ee80
Vanzhere <vaniya.sharma158@gmail.com>
2026-07-30 05:09:24 +0000

    update

--- mathobs.java
+++ mathobs.java
@@ -12,3 +12,14 @@ class Solution {
 }
 
 // LC 191 number of 1 bits
+class Solution {
+    public int hammingWeight(int n) {
+        int count = 0;
+        while(n!=0){
+            count+= n&1;
+            n >>>= 1;
+        }
+        return count;
+        
+    }
+}
