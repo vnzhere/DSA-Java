8751490121e07d9fe136d74cc325de55cfc76193
Vanzhere <vaniya.sharma158@gmail.com>
2026-07-30 05:06:47 +0000

    update

---
+++ mathobs.java
@@ -0,0 +1,12 @@
+// LC 172 factorial trailing zeroes
+class Solution {
+    public int trailingZeroes(int n) {
+        int count = 0;
+        while( n >= 5){
+            n/=5;
+            count+=n;
+        }
+        return count;
+        
+    }
+}
