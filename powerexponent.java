3b13bd33cb00d433284c7996fd965644fad9c10a
Vanzhere <vaniya.sharma158@gmail.com>
2026-07-30 03:29:03 +0000

    update

--- powerexponent.java
+++ powerexponent.java
@@ -16,3 +16,25 @@ while(N > 0){
 return result;
 
 // LC 50 
+class Solution {
+    public double myPow(double x, int n) {
+
+        long N = n;
+
+        if(N < 0){
+            x= 1/x;
+            N = -N;
+        }
+
+        double result = 1;
+        while(N > 0){
+            if(N % 2 ==1){
+                result *= x;
+            }
+            x *= x;
+            N = N/2;
+        }
+        return result;
+        
+    }
+}
