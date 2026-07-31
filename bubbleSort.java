9027bb225095d43b2b68bfbdc70bcf8c5951f41d
Vanzhere <vaniya.sharma158@gmail.com>
2026-07-31 04:43:34 +0000

    update

--- bubbleSort.java
+++ bubbleSort.java
@@ -26,3 +26,19 @@ System.out.println(Arrays.toString(arr2));
 // [1, 2, 3, 4] → 0 swaps, exits early
 
 // LC 283 Move zeroes
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left = 0;
+
+        for(int right = 0; right< nums.length; right++){
+            if(nums[right]!=0){
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+
+                left++;
+            }
+        }
+        
+    }
+}
