449e1d9b6ba612f1429afa8613a893bff88ffc7d
Vanzhere <vaniya.sharma158@gmail.com>
2026-08-06 18:23:06 +0000

    update

---
+++ mergesort.java
@@ -0,0 +1,3 @@
+echo "alias push='git add . && git commit -m \"update\" && git push'" >> ~/.bashrc
+source ~/.bashrcecho "alias push='git add . && git commit -m \"update\" && git push'" >> ~/.bashrc
+source ~/.bashrc
