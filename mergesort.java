f54a95595005c134e86eb79eb7edefa4149dc255
Vanzhere <vaniya.sharma158@gmail.com>
2026-08-10 13:43:43 +0000

    Initial Commit

--- mergesort.java
+++ mergesort.java
@@ -8,3 +8,9 @@ class Solution {
             if(left >= right){
             return;
         }  
+        int mid = (left+right)/2;
+        mergesort(arr, left, mid);
+        mergesort(arr, mid+1, right);
+        merge(arr, left, mid, right);
+        
+    }
