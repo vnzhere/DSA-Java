79a234899bd888d6e6f967cfeb7e2508e6c2cab2
Vanzhere <vaniya.sharma158@gmail.com>
2026-08-10 13:42:05 +0000

    Initial Commit

--- mergesort.java
+++ mergesort.java
@@ -6,11 +6,5 @@
 
 
  echo "alias push='git add . && git commit -m \"update\" && git push'" >> ~/.bashrc
-@vnzhere ➜ /workspaces/DSA-Java (main) $  echo "alias push='git add . && git commit -m \"update\" && git push'" >> ~/.bashrc
-@vnzhere ➜ /workspaces/DSA-Java (main) $  echo "alias push='git add . && git commit -m \"update\" && git push'" >> ~/.bashrc
-@vnzhere ➜ /workspaces/DSA-Java (main) $  echo "alias push='git add . && git commit -m \"update\" && git push'" >> ~/.bashrc
-@vnzhere ➜ /workspaces/DSA-Java (main) $  echo "alias push='git add . && git commit -m \"update\" && git push'" >> ~/.bashrc
-@vnzhere ➜ /workspaces/DSA-Java (main) $  echo "alias push='git add . && git commit -m \"update\" && git push'" >> ~/.bashrc
-@vnzhere ➜ /workspaces/DSA-Java (main) $  echo "alias push='git add . && git commit -m \"update\" && git push'" >> ~/.bashrc
-@vnzhere ➜ /workspaces/DSA-Java (main) $ 
+
 
