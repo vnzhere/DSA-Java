00458e48e718180d11a27a755e8cc56f5ffadd10
Vanzhere <vaniya.sharma158@gmail.com>
2026-08-17 16:24:06 +0000

    Initial COmmit

--- selectionsort.java
+++ selectionsort.java
@@ -16,4 +16,7 @@ source ~/.bashrcecho "alias push='git add . && git commit -m \"Initial COmmit\" 
 source ~/.bashrcecho "alias push='git add . && git commit -m \"Initial COmmit\" && git push'" >> ~/.bashrc
 source ~/.bashrcecho "alias push='git add . && git commit -m \"Initial COmmit\" && git push'" >> ~/.bashrc
 source ~/.bashrcecho "alias push='git add . && git commit -m \"Initial COmmit\" && git push'" >> ~/.bashrc
+source ~/.bashrcecho "alias push='git add . && git commit -m \"Initial COmmit\" && git push'" >> ~/.bashrc
+source ~/.bashrcecho "alias push='git add . && git commit -m \"Initial COmmit\" && git push'" >> ~/.bashrc
+source ~/.bashrcecho "alias push='git add . && git commit -m \"Initial COmmit\" && git push'" >> ~/.bashrc
 source ~/.bashrc
