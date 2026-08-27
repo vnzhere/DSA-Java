32016dbbf67fa8a1dbd9efb5544bcaf39792ab3e
Vanzhere <vaniya.sharma158@gmail.com>
2026-08-27 14:59:49 +0000

    Initial COmmit

--- selectionsort.java
+++ selectionsort.java
@@ -14,3 +14,37 @@ for (int i =0; i < n-1; i++){
 }
 
 // LC - 414 Third distinct number
+class Solution {
+    public int thirdMax(int[] nums) {
+
+        long max1 = Long.MIN_VALUE;
+        long max2 = Long.MIN_VALUE;
+        long max3 = Long.MIN_VALUE;
+
+
+        for(int num: nums){
+
+            if (num == max1 || num == max2 || num == max3)
+            continue;
+
+            if(num > max1){
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            }
+
+            else if(num > max2){
+                max3 = max2;
+                max2 = num;
+            }
+
+            else if(num> max3){
+                max3 = num;
+            }
+        }
+
+        return max3 == Long.MIN_VALUE ? (int) max1 : (int) max3;
+        
+        
+    }
+}
