00164cc414df59c9e5c884eaecd7d825bae4029d
Vanzhere <vaniya.sharma158@gmail.com>
2026-08-30 18:25:22 +0000

    Initial COmmit

--- insertionsort.java
+++ insertionsort.java
@@ -19,3 +19,13 @@ echo "alias push='git add . && git commit -m \"Initial COmmit\" && git push'" >>
 source ~/.bashrc
 echo "alias push='git add . && git commit -m \"Initial COmmit\" && git push'" >> ~/.bashrc
 source ~/.bashrc
+echo "alias push='git add . && git commit -m \"Initial COmmit\" && git push'" >> ~/.bashrc
+source ~/.bashrc
+echo "alias push='git add . && git commit -m \"Initial COmmit\" && git push'" >> ~/.bashrc
+source ~/.bashrc
+echo "alias push='git add . && git commit -m \"Initial COmmit\" && git push'" >> ~/.bashrc
+source ~/.bashrc
+echo "alias push='git add . && git commit -m \"Initial COmmit\" && git push'" >> ~/.bashrc
+source ~/.bashrc
+echo "alias push='git add . && git commit -m \"Initial COmmit\" && git push'" >> ~/.bashrc
+source ~/.bashrc
