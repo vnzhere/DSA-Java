bc679555c62aa14518d4e55c5585814a6940e95b
Vanzhere <vaniya.sharma158@gmail.com>
2026-09-10 12:19:05 +0000

    Initial COmmit

--- recursion.java
+++ recursion.java
@@ -4,3 +4,12 @@ static long factorial (int n){
 
     return n * factorial(n-1);
 }
+
+
+// p-2 multiple recursion calls
+static int fibonacci(int n){
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+
+    return fibonacci(n-1)+ fibonacci(n-2);
+}
