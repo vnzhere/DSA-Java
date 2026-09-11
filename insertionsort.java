fd2185e279ced9b6d27ddcd544a8619a1d1269c7
Vanzhere <vaniya.sharma158@gmail.com>
2026-09-11 18:21:19 +0000

    Initial COmmit

---
+++ insertionsort.java
@@ -0,0 +1,29 @@
+//  P-1: simple recursion with return value 
+static long factorial (int n){
+    if( n <= 1) return 1;
+
+    return n * factorial(n-1);
+}
+
+
+// p-2 multiple recursion calls
+static int fibonacci(int n){
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+
+    return fibonacci(n-1)+ fibonacci(n-2);
+}//  P-1: simple recursion with return value 
+static long factorial (int n){
+    if( n <= 1) return 1;
+
+    return n * factorial(n-1);
+}
+
+
+// p-2 multiple recursion calls
+static int fibonacci(int n){
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+
+    return fibonacci(n-1)+ fibonacci(n-2);
+}
