80f3453d9e709145b2470e6d74e1b6c562f10db0
Vanzhere <vaniya.sharma158@gmail.com>
2026-09-15 18:09:37 +0000

    Initial Commit

--- Quicksort.java
+++ Quicksort.java
@@ -3,3 +3,10 @@ static void quickSort(int[] arr, int low, int high) {
     if (low < high) {
         // partition and get pivot index
         int pivotIndex = partition(arr, low, high);
+
+         // sort left of pivot
+        quickSort(arr, low, pivotIndex - 1);
+
+        // sort right of pivot
+        quickSort(arr, pivotIndex + 1, high);
+    }
