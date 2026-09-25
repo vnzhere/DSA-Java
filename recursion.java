48462ac9abe02fff2394529fd7e048de0048cce0
Vanzhere <vaniya.sharma158@gmail.com>
2026-09-25 18:26:05 +0000

    Initial Commit

--- recursion.java
+++ recursion.java
@@ -14,3 +14,33 @@ static int fibonacci(int n){
     return fibonacci(n-1)+ fibonacci(n-2);
 }
 
+Arrays.sort(nums);
+
+for (int i = 0; i < nums.length - 2; i++) {
+    int left = i + 1;
+    int right = nums.length - 1;
+
+    while (left < right) {
+        int sum = nums[i] + nums[left] + nums[right];
+
+        if (sum == 0) {
+            // found triplet
+        } else if (sum < 0) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+}
+
+
+
+
+
+
+echo "alias push='git add . && git commit -m \"Initial Commit\" && git push'" >> ~/.bashrc
+source ~/.bashrcecho "alias push='git add . && git commit -m \"Initial Commit\" && git push'" >> ~/.bashrc
+source ~/.bashrcecho "alias push='git add . && git commit -m \"Initial Commit\" && git push'" >> ~/.bashrc
+source ~/.bashrcecho "alias push='git add . && git commit -m \"Initial Commit\" && git push'" >> ~/.bashrc
+source ~/.bashrcecho "alias push='git add . && git commit -m \"Initial Commit\" && git push'" >> ~/.bashrc
+source ~/.bashrc
